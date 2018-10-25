59259e343d371599a011c282929155219b1ee655
SlavaChe <slavichmail@gmail.com>
2018-10-25 19:36:46 +0700

    Remove unnecessary tags in the header

--- content/themes/default-theme/functions.php
+++ content/themes/default-theme/functions.php
@@ -423,3 +423,27 @@ function filter_plugin_updates( $update ) {
 }
 
 // add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
+
+/*
+* Удалить ненужные теги в шапке
+*/
+// remove favicon site
+remove_action('wp_head', 'wp_site_icon', 99 );
+// remove dns-prefetch
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
+// Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+// Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'feed_links', 2 );
+// Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'rsd_link' );
+// Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'wlwmanifest_link' );
+// index link
+remove_action( 'wp_head', 'index_rel_link' );
+// prev link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+// start link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+// Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
